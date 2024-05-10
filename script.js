ef149cbf0b1cb1cb5967c5d95e766ef59c1631e6
Your Name <jamaltaibi@mbp-de-jamal.home>
2024-05-10 15:14:11 +0200

    Portfolio

--- script.js
+++ script.js
@@ -9,14 +9,36 @@ function openNav() {
   sidenav.classList.add("active");
   window.addEventListener("scroll", closeNavOnScroll);
 }
-
 function closeNav() {
   sidenav.classList.remove("active");
 }
-
 function closeNavOnScroll() {
   if (sidenav.classList.contains("active")) {
     sidenav.classList.remove("active");
     window.removeEventListener("scroll", closeNavOnScroll);
   }
 }
+
+
+const modalTriggers = document.querySelectorAll('.modal-trigger');
+const modal = document.getElementById("myModal");
+
+modalTriggers.forEach(function(trigger) {
+    trigger.addEventListener('click', function() {
+        modal.style.display = "block"; 
+    });
+});
+
+const closeBt = document.querySelector('.close');
+
+closeBt.addEventListener('click', function() {
+    modal.style.display = "none";
+});
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"; 
+    }
+});
+
+
+
